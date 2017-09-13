2e736e7149f5cd2a1cc7729d420227f5bb327e47
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2017-09-13 17:07:17 +0200

    forgot to add tests...

--- src/wavelet_trie/tests.rs
+++ src/wavelet_trie/tests.rs
@@ -3,34 +3,34 @@ mod tests {
 	use bit_vec_wrap::BitVecWrap;
 	use wavelet_trie::WaveletTrie;
 
-//	#[test]
-//	fn insert_one_sequence() {
-//		let sequence = BitVecWrap::from_bytes(&[0b00001000]);
-//		let mut wt = WaveletTrie::new();
-//		wt.insert_static(&[sequence]);
-//		println!("{:?}", wt);
-//		// TODO: assert; wait for "rank"
-//	}
-//
-//	#[test]
-//	fn insert_same_sequences() {
-//		let sequence1 = BitVecWrap::from_bytes(&[0b00001000]);
-//		let sequence2 = BitVecWrap::from_bytes(&[0b00001000]);
-//		let mut wt = WaveletTrie::new();
-//		wt.insert_static(&[sequence1, sequence2]);
-//		println!("{:?}", wt);
-//		// TODO: assert; wait for "rank"
-//	}
-//
-//	#[test]
-//	fn insert_two_different_sequences() {
-//		let sequence1 = BitVecWrap::from_bytes(&[0b00001000]);
-//		let sequence2 = BitVecWrap::from_bytes(&[0b00000001]);
-//		let mut wt = WaveletTrie::new();
-//		wt.insert_static(&[sequence1, sequence2]);
-//		println!("{:?}", wt);
-//		// TODO: assert; wait for "rank"
-//	}
+	#[test]
+	fn insert_one_sequence() {
+		let sequence = BitVecWrap::from_bytes(&[0b00001000]);
+		let mut wt = WaveletTrie::new();
+		wt.insert_static(&[sequence]);
+		println!("{:?}", wt);
+		// TODO: assert; wait for "rank"
+	}
+
+	#[test]
+	fn insert_same_sequences() {
+		let sequence1 = BitVecWrap::from_bytes(&[0b00001000]);
+		let sequence2 = BitVecWrap::from_bytes(&[0b00001000]);
+		let mut wt = WaveletTrie::new();
+		wt.insert_static(&[sequence1, sequence2]);
+		println!("{:?}", wt);
+		// TODO: assert; wait for "rank"
+	}
+
+	#[test]
+	fn insert_two_different_sequences() {
+		let sequence1 = BitVecWrap::from_bytes(&[0b00001000]);
+		let sequence2 = BitVecWrap::from_bytes(&[0b00000001]);
+		let mut wt = WaveletTrie::new();
+		wt.insert_static(&[sequence1, sequence2]);
+		println!("{:?}", wt);
+		// TODO: assert; wait for "rank"
+	}
 
 	#[test]
 	fn insert_different_sequences() {
