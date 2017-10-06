511d3988611fff157d128d278a456e9f02d25968
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2017-10-06 16:17:54 +0200

    remove comments on pointer messing; we're not going to use that.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,2 @@
 pub mod wavelet_trie;
 mod bit_vec_wrap;
-
-// note: moving pointers in rust:
-// let x = Box::new(5);
-// let ptr = Box::into_raw(x);
-// let x = unsafe { Box::from_raw(ptr) };
-// or can we move the box as such?
